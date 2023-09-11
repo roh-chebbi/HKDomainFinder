2efc9b0c4d45c382ec523fd5fe5ac896a0b958a9
rohinichebbi <rohinichebbi@gmail.com>
2023-09-11 23:28:41 +0530

    Redirect matches to new file

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,7 +4,7 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-tblastn -query $1 -subject $2 -outfmt 6 | awk '{ if ($3 > 30 && $4>0.9*($8-$7)) {print $0} }' 
+tblastn -query $1 -subject $2 -outfmt 6 | awk '{ if ($3 > 30 && $4>0.9*($8-$7)) {print $0} }' > $3 
 
 approx_matches=`wc -l "$3" | cut -d" " -f1`
 
