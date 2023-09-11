fa44b2c657638baa776437a170bd01c573ca9dd9
rohinichebbi <rohinichebbi@gmail.com>
2023-09-11 23:15:26 +0530

    Print matches to terminal

--- find_homologs.sh
+++ find_homologs.sh
@@ -6,3 +6,6 @@ output_file=$3
 
 tblastn -query $1 -subject $2 -outfmt 6 | awk '{ if ($3 > 30 && $4>0.9*($8-$7)) {print $0} }' 
 
+approx_matches=`wc -l "$3" | cut -d" " -f1`
+
+echo "Number of approx matches in $1 is: $approx_matches"
