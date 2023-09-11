8e326c2efaf0543bf0bb3b77d08a97025ec8d460
rohinichebbi <rohinichebbi@gmail.com>
2023-09-11 23:08:06 +0530

    Change similarity and length criteria

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,9 +4,5 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-blastn -query $1 -subject $2 -task blastn-short -outfmt 6 | awk '$3==100 && $4==28 {print $0}' > $3 
+tblastn -query $1 -subject $2 -outfmt 6 | awk '{ if ($3 > 30 && $4>0.9*($8-$7)) {print $0} }' 
 
-perfect_matches=`wc -l "$3" | cut -d" " -f1`
-
-echo "Number of perfect matches in $1 is: $perfect_matches"
-cat $3
