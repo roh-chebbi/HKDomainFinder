29d148e033eee5f46e567e90acbc712f4126a123
rohinichebbi <rohinichebbi@gmail.com>
2023-09-13 07:04:15 +0530

    Changed match criteria

--- find_homologs.sh
+++ find_homologs.sh
@@ -4,8 +4,8 @@ query_file=$1
 subject_file=$2
 output_file=$3
 
-tblastn -query $1 -subject $2 -outfmt 6 | awk '{ if ($3 > 30 && $4>0.9*($8-$7)) {print $0} }' > $3 
+tblastn -query $1 -subject $2 -outfmt "6 std qlen" | awk '{ if ($3 > 30 && $4>0.9*($13)) {print $0} }' > $3 
 
 approx_matches=`wc -l "$3" | cut -d" " -f1`
 
-echo "Number of approx matches in $1 is: $approx_matches"
+echo -e "Number of approx matches in $2 is: $approx_matches\n"
